e98fec34e660ae4be44818ce84c67a0170d1df05
Jesus Lopez <jesuslopezsilva206@gmail.com>
2024-05-24 13:32:24 -0600

    Inicio del proyecto.

---
+++ src/pkg2024_itess_itess_ti202_ci_portafolio/Main.java
@@ -0,0 +1,19 @@
+/*
+* ITESS-TICS-2024
+* ENERO-JUNIO 2024
+* CALCULO INTEGRAL
+* TI202 JLS
+* 23 DE FEBRERO DEL 2024
+* Portafolio 
+* Proyecto Java Fx
+* jesuslopezsilva206@gmail.com
+ */
+package pkg2024_itess_itess_ti202_ci_portafolio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
